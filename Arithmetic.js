44a3c40c2b25bdbe4f42ee32f73512497105e5e0
Bhavya-oss <77015284+Bhavya-oss@users.noreply.github.com>
2022-12-19 20:17:37 +0530

    A simple calculator program
    Perform a simple arihtmetic operation on the given operands and operator

---
+++ Arithmetic.js
@@ -0,0 +1,18 @@
+function Arithmetic(){
+    let a="+";
+    let b=2;
+    let c=1;
+    switch(a)
+    {
+        case "+":console.log(c+b);
+        break;
+        case "-":console.log(c-b);
+        break;
+        case "*":console.log(c*b);
+        break;
+        case "/":console.log(c/b);
+        break;
+     default: console.log("Invalid")
+    }
+}
+Arithmetic();
