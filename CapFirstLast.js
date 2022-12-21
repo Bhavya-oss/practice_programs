ba9061560c10d4c4fa8101e94b2f265c81244243
Bhavya-oss <77015284+Bhavya-oss@users.noreply.github.com>
2022-12-21 21:06:24 +0530

    Add files via upload

---
+++ CapFirstLast.js
@@ -0,0 +1,27 @@
+function FirstAndLast(str) {
+
+    // Create an equivalent string
+    // of the given string
+    var ch = str.split('');
+    for (var i = 0; i < ch.length; i++) {
+
+        // k stores index of first character
+        // and i is going to store index of last
+        // character.
+        var k = i;
+        while (i < ch.length && ch[i] != ' ')
+            i++;
+
+        // Check if the character is a small letter
+        // If yes, then Capitalise
+        ch[k] = String.fromCharCode(ch[k] >= 'a' &&
+            ch[k] <= 'z' ? (ch[k].charCodeAt(0) - 32)
+            : ch[k].charCodeAt(0));
+        ch[i - 1] = String.fromCharCode(ch[i - 1] >= 'a'
+            && ch[i - 1] <= 'z' ? (ch[i - 1].charCodeAt(0) - 32)
+            : ch[i - 1].charCodeAt(0));
+    }
+
+    console.log(ch.join(''));
+}
+FirstAndLast("i bound can do")
