92d121c6d60ae770ab78fd4e74bce087b41a5d69
Bhavya-oss <77015284+Bhavya-oss@users.noreply.github.com>
2022-12-21 22:35:39 +0530

    Add files via upload

--- Decimaltoromannumber.js
+++ Decimaltoromannumber.js
@@ -5,83 +5,83 @@ function Decimaltoromannber(n)
 
         if (n >= 1000)       // 1000 - m
         {
-           console.log("m");
+         process.stdout.write("m");
            n -= 1000;
         }
 
         else if (n >= 900)   // 900 -  cm
         {
-           console.log("cm");
+         process.stdout.write("cm");
            n -= 900;
         }        
 
         else if (n >= 500)   // 500 - d
         {           
-           console.log("d");
+         process.stdout.write("d");
            n -= 500;
         }
 
         else if (n >= 400)   // 400 -  cd
         {
-           console.log("cd");
+         process.stdout.write("cd");
            n -= 400;
         }
 
         else if (n >= 100)   // 100 - c
         {
-           console.log("c");
+         process.stdout.write("c");
            n -= 100;                       
         }
 
         else if (n >= 90)    // 90 - xc
         {
-           console.log("xc");
+         process.stdout.write("xc");
            n -= 90;                                              
         }
 
         else if (n >= 50)    // 50 - l
         {
-           console.log("l");
+         process.stdout.write("l");
            n -= 50;                                                                     
         }
 
         else if (n >= 40)    // 40 - xl
         {
-           console.log("xl");           
+         process.stdout.write("xl");           
            n -= 40;
         }
 
         else if (n >= 10)    // 10 - x
         {
-           console.log("x");
+         process.stdout.write("x");
            n -= 10;           
         }
 
         else if (n >= 9)     // 9 - ix
         {
-           console.log("ix");
+         process.stdout.write("ix");
            n -= 9;                         
         }
 
         else if (n >= 5)     // 5 - v
         {
-           console.log("v");
+         process.stdout.write("v");
           n -= 5;                                     
         }
 
         else if (n >= 4)     // 4 - iv
         {
-           console.log("iv");
+         process.stdout.write("iv");
            n -= 4;                                                            
         }
 
         else if (n >= 1)     // 1 - i
         {
-           console.log("i");
+         process.stdout.write("i");
            n -= 1;                                                                                   
         }
 
     }
 }
-Decimaltoromannber(10)
+Decimaltoromannber(19)
 
